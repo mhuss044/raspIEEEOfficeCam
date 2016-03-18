5c66daf4173555fac435680f155e992a4f5727fc
pi <paemaili>
2016-03-18 17:32:08 +0000

    buzzer code works

--- buzz.py
+++ buzz.py
@@ -1,19 +1,26 @@
 import RPi.GPIO as GPIO
 import time
 def startMotor():
-	GPIO.setmode(GPIO.BCM)
+	GPIO.setmode(GPIO.BCM)		# broadcom pin numbering scheme
 	GPIO.setwarnings(False)
 
 	buzzer = 18
+	motor = 25
+	duty = 10
+	GPIO.setup(motor, GPIO.OUT)
 	GPIO.setup(buzzer, GPIO.OUT)
+	pwm = GPIO.PWM(buzzer, 850) # init pwm, on buzzerPin, f = 100hz
 
 	try:
-		GPIO.output(buzzer, True)
+		pwm.start(duty)	# duty cycle set 0 - 100
+		pwm.ChangeDutyCycle(duty)
+		GPIO.output(motor, True)
 		time.sleep(3)
-		GPIO.output(buzzer, False)
+		GPIO.output(motor, False)
 	except:
 		pass
 	finally:
+		pwm.stop()
 		GPIO.cleanup()
 
 
